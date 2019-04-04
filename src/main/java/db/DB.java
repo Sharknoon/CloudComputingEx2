faeae5ab7bb0f4d9ce8fed834f84c09a4419f93e
Josua Frank <frank.josua@gmail.com>
2019-04-04 17:15:08 +0200

    db

--- src/main/java/db/DB.java
+++ src/main/java/db/DB.java
@@ -3,7 +3,11 @@ package db;
 import java.util.List;
 
 public interface DB {
-    
+
+    default DB getInstance(){
+        return null;
+    }
+
     void storeImage(Image image);
 
     List<Image> loadImages();
